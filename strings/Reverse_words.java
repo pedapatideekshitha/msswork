ceb25d528e91fcc5c9f4c61b4aabc7d5325c748e
Deekshitha Pedapati <93853405+pedapatideekshitha@users.noreply.github.com>
2024-05-29 19:26:02 +0530

    Add files via upload

---
+++ strings/Reverse_words.java
@@ -0,0 +1,20 @@
+package strings;
+import java.util.*;
+public class Reverse_words {
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a string");
+String s=sc.nextLine();
+
+String a[]=s.split(" ");
+
+String str="";
+
+for(int i=a.length-1;i>=0;i--)
+{
+str=str+a[i]+" ";
+}
+System.out.println(str);
+}
+}
